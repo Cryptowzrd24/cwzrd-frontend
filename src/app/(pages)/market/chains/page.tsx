7dc6058322be0e96e57e96f6cae386dea9502db7
hamid rafiq <hamidrafiq962@gmail.com>
2024-06-15 01:05:59 +0500

    feat(chains): created chains page

---
+++ src/app/(pages)/market/chains/page.tsx
@@ -0,0 +1,12 @@
+import Chains from '@/app/components/chains';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <Chains />
+    </div>
+  );
+};
+
+export default page;
