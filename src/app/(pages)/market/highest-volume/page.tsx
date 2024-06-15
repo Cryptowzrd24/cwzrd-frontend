02f15a9560640b6bff5713c22662d0766aa35161
hamid rafiq <hamidrafiq962@gmail.com>
2024-06-15 15:03:00 +0500

    feat(highest-volume): created highest-volume page

---
+++ src/app/(pages)/market/highest-volume/page.tsx
@@ -0,0 +1,12 @@
+import HighestVolume from '@/app/components/highest-volume';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <HighestVolume />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/components/highest-volume/index.tsx
@@ -0,0 +1,36 @@
+import { Container, Box } from '@mui/material';
+import React from 'react';
+import HeroContent from './heroContent';
+import Hero from '../hero-section/hero';
+import NewsLetterBanner from '../banners/newsLetterBanner';
+import NewsLetter from '../news-letter';
+import Technicals from '../technicals';
+import MemberShipBanner from '../banners/memberShipBanner';
+
+const HighestVolume = () => {
+  return (
+    <>
+      <Container maxWidth="xl" sx={{ overflow: 'hidden' }}>
+        <Box sx={{ mt: '80px' }}>
+          <Hero />
+          <HeroContent />
+        </Box>
+        <Box sx={{ mt: '80px', mb: '80px' }}>
+          <NewsLetterBanner />
+        </Box>
+        <Box sx={{ mt: '80px' }}>
+          <NewsLetter />
+        </Box>
+        <Box sx={{ mt: '80px' }}>
+          <Technicals />
+        </Box>
+
+        <Box sx={{ mt: '220px', mb: '120px' }}>
+          <MemberShipBanner />
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default HighestVolume;
