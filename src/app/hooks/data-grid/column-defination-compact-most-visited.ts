7114535a79f85259075446540c775e0237c71ea5
arham4312 <arhamnadeem4312@gmail.com>
2024-06-21 17:06:12 +0500

    feat(spotlight grid): complete spotlight table

---
+++ src/app/hooks/data-grid/column-defination-compact-most-visited.ts
@@ -0,0 +1,55 @@
+import { useMemo } from 'react';
+import { CurrencyNameComponent } from '@/app/components/data-table/name';
+import { ID } from '@/app/components/data-table/id';
+
+import { priceNumberFormatter } from '@/utils/price-number-formater';
+import { getPercentStyle } from '@/utils/profit-loss-color';
+import { profitLossCheck } from '@/utils/profit-loss-val-check';
+
+import '../../../app/styles/new-coins.css';
+
+const useColumnCompactMostVisitedDefs = (columns: any) => {
+  return useMemo(() => {
+    return columns.map((col: any) => {
+      switch (col.field) {
+        case 'index':
+          return {
+            field: 'index',
+            headerName: '#',
+            cellRenderer: ID,
+            width: 70,
+          };
+        case 'name':
+          return {
+            field: 'name',
+            headerName: 'Name',
+            cellRenderer: CurrencyNameComponent,
+            width: 160,
+          };
+        case 'price':
+          return {
+            field: 'price',
+            headerName: 'Price',
+            width: 95,
+            valueFormatter: (p: any) => '$' + priceNumberFormatter(p.value),
+          };
+        case 'percent_change_24h':
+          return {
+            field: col.field,
+            headerName: col.headerName,
+            width: 100,
+            cellStyle: (p: any) => getPercentStyle(p.value),
+            valueFormatter: (p: any) => {
+              const value = p.value;
+              const formattedValue = priceNumberFormatter(value) + ' %';
+              return profitLossCheck(formattedValue);
+            },
+          };
+        default:
+          return col;
+      }
+    });
+  }, [columns]);
+};
+
+export default useColumnCompactMostVisitedDefs;
