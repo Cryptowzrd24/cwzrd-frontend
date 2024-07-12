240f9cf74be3b90ce32694f049df20b16753b03d
Hamzarehman11 <Hamzarehman.1175@gmail.com>
2024-07-12 16:50:39 +0500

    cards(cards Ui): added cards in gainer and losers page

--- src/app/components/gainers-losers/heroContent.tsx
+++ src/app/components/gainers-losers/heroContent.tsx
@@ -1,6 +1,13 @@
 'use client';
 import { Box, Typography } from '@mui/material';
+import { AnimatePresence, motion } from 'framer-motion';
 import React, { useState } from 'react';
+import GaugeChart from '../common/guage-chart';
+import GraphCardTop from '../common/graph-card-top';
+import { areaChartData } from '@/app/constants/charts';
+import Card3 from '../common/card3.component';
+import { colorConfig } from '@/app/helpers/config';
+import cardBgImage7 from '@/app/assets/images/cardImagebg7.png';
 
 function HeroContent() {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -52,6 +59,47 @@ function HeroContent() {
           {isExpanded ? ' Read Less' : ' Read More'}
         </span>
       </Box>
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0, height: 0, marginTop: 0 }}
+          animate={{ opacity: 1, height: 'auto', marginTop: '48px' }}
+          exit={{ opacity: 0, height: 0 }}
+          transition={{ duration: 0.2, marginTop: 0 }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              gap: '10px',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <GraphCardTop
+              heading="🚀 Top Gain"
+              percent={0.32}
+              graphAttr={{ type: 'area', data: areaChartData }}
+            />
+            <GraphCardTop
+              heading="🚨 Top Losers"
+              percent={-0.32}
+              graphAttr={{ type: 'area', data: areaChartData }}
+            />
+            <Card3
+              transparentButton={true}
+              bgImage={cardBgImage7}
+              staticTextColor={colorConfig.white}
+              textColor={colorConfig.green}
+              heading="Top Gain"
+              name="Dogwifhat"
+              desc="took the first place with a gain of"
+              value="+29.32%"
+              end="in 7 days."
+            />
+            <GaugeChart value={0} />
+          </Box>
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
