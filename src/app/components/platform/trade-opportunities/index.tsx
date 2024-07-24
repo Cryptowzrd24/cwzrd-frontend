6b0dd2eb60c3e3685f83d5cc87f72baea62308e5
hamid rafiq <hamidrafiq962@gmail.com>
2024-07-24 19:38:42 +0500

    feat(platform page)added features and trade

--- src/app/components/platform/trade-opportunities/index.tsx
+++ src/app/components/platform/trade-opportunities/index.tsx
@@ -1,12 +1,16 @@
 import { Box } from '@mui/material';
 import React from 'react';
 import TradeHeader from './tradeHeader';
+import TradeCards from './tradeCards';
 
 const TradeOpportunities = () => {
   return (
     <>
       <Box sx={{ mb: '72px' }}>
         <TradeHeader />
+      </Box>
+      <Box sx={{ mb: '72px' }}>
+        <TradeCards />
       </Box>
     </>
   );
