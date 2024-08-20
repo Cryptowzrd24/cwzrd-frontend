9b4d45cefcba60a0051f2f317b79cc8f1c5bb21a
Hamzarehman11 <Hamzarehman.1175@gmail.com>
2024-08-20 13:01:50 +0500

    fix(coin details): changing to filters when market cap is selected

--- src/app/components/coin-details/graph-layout/index.tsx
+++ src/app/components/coin-details/graph-layout/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './index.module.css';
 import StockChart from '../stock-chart';
 import GraphCustomHeader from '../graph-custom-header';
@@ -24,6 +24,11 @@ function GraphLayout() {
       }
     }
   };
+  useEffect(() => {
+    if (selectedTab === 'Market Cap') {
+      setSelectedFilter('filter');
+    }
+  }, [selectedTab]);
   return (
     <div className={styles.graphLayout}>
       <Box
