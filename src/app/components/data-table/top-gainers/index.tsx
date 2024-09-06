e38faf70755208dd6044f01f596ee5b56e7862fc
Muhammad Abbas Bangash <mabbasbangash97@gmail.com>
2024-09-06 16:13:17 +0500

    integrated coin price with websocket in share coin details

--- src/app/components/data-table/top-gainers/index.tsx
+++ src/app/components/data-table/top-gainers/index.tsx
@@ -4,21 +4,12 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 export const TopGainers = ({ data }: { data: any }) => {
-  const handleMouseEnter = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.currentTarget.querySelector('img')!.style.transform = 'scale(1.1)';
-  };
-
-  const handleMouseLeave = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.currentTarget.querySelector('img')!.style.transform = 'scale(1)';
-  };
   return (
     <div className={styles['top-gainers']}>
       {data.top_gainers.map((id: number) => (
         <Link
           style={{ textDecoration: 'none' }}
           href={`/market/coin-details/${id}`}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
         >
           <Image
             key={id}
