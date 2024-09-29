37b4cb98e18d486e37664feb3f4081a0f097b6d2
arham4312 <arhamnadeem4312@gmail.com>
2024-09-29 22:28:58 +0500

    minor fixes

--- src/app/components/data-table/market-data/hash/index.tsx
+++ src/app/components/data-table/market-data/hash/index.tsx
@@ -3,14 +3,13 @@
 import React from 'react';
 import { CustomCellRendererProps } from 'ag-grid-react';
 import { Box } from '@mui/material';
-import StarRank from '../../../../../../public/icons/nft/starRank';
 
 export const Hash = (props: CustomCellRendererProps) => {
   const { value } = props;
 
   return (
     <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-      <StarRank />
+      {/* <StarRank /> */}
       <div>{value}</div>
     </Box>
   );
