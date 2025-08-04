4749e61a04adcf120eb87006a6fc3750df10fd01
sheharyar-folium <muhammad.shehryar@folium.ai>
2025-08-04 21:13:46 +0500

    resolved build issue

--- src/app/components/filter-dropdown/index.tsx
+++ src/app/components/filter-dropdown/index.tsx
@@ -55,41 +55,46 @@ function FilterDropdown({
       anchorEl={anchorEl}
       open={open}
       onClose={handleClose}
-      PaperProps={{
-        style: {
-          maxHeight: '472px',
-          width: '295px',
-          borderRadius: '16px',
-          overflowY: 'scroll',
-          padding: '14px',
-          boxShadow: '0px 4px 28px 0px rgba(0, 0, 0, 0.05)',
-          '@media (max-width: 600px)': {
-            width: '280px',
-            padding: '12px',
-            maxHeight: '400px',
-          },
-          '@media (max-width: 480px)': {
-            width: '260px',
-            padding: '10px',
-            maxHeight: '350px',
+      slotProps={{
+        paper: {
+          sx: {
+            maxHeight: '472px',
+            width: '295px',
+            borderRadius: '16px',
+            overflowY: 'scroll',
+            padding: '14px',
+            boxShadow: '0px 4px 28px 0px rgba(0, 0, 0, 0.05)',
+            '@media (max-width: 600px)': {
+              width: '280px',
+              padding: '12px',
+              maxHeight: '400px',
+            },
+            '@media (max-width: 480px)': {
+              width: '260px',
+              padding: '10px',
+              maxHeight: '350px',
+            },
           },
         },
       }}
     >
       <Box sx={styles.searchContainer}>
         <FilterSearchIcon strokeOpacity={'0.5'} />
-        <input
-          style={styles.searchInput}
+        <Box
+          component="input"
+          sx={styles.searchInput}
           placeholder={'Search...'}
           onChange={(e: any) => setSearchString(e.target.value)}
         />
       </Box>
-      <Box sx={{ 
-        padding: {
-          xs: '16px 8px',
-          sm: '20px 10px',
-        },
-      }}>
+      <Box
+        sx={{
+          padding: {
+            xs: '16px 8px',
+            sm: '20px 10px',
+          },
+        }}
+      >
         <Typography
           sx={styles.dropdownSubheading}
           id="modal-modal-title"
